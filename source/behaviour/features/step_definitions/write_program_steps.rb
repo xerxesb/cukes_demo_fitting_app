4f41e0edd24f9f77223cc0730e01fcacfa91d9cc
Xerxes Battiwalla <xbattiwalla@cochlear.com>
2009-09-22 15:48:03 +1000

    Implemented cucumber specification for WriteProgramService

--- source/behaviour/features/step_definitions/write_program_steps.rb
+++ source/behaviour/features/step_definitions/write_program_steps.rb
@@ -1,11 +1,29 @@
+require 'd:\\source\\cochlear\\simplefit\\source\\behaviour\\specs\\GlobalInclude.rb'
+require 'spec'
+
+class CdiService
+    include Services::ICdiService
+  
+    attr_reader :write_program_was_called
+      
+    def write_program
+        @write_program_was_called = true;
+    end    
+end
+
 Given /^a new program$/ do
-  pending
+  #pending
 end
 
 When /^I write the program$/ do
-  pending
+  
+    @cdi_service = CdiService.new
+    
+    write_program_service = Services::WriteProgramService.new @cdi_service
+    write_program_service.write_program
+    
 end
 
 Then /^it should be in slot 1$/ do
-  pending
+    @cdi_service.write_program_was_called.should be_true
 end
